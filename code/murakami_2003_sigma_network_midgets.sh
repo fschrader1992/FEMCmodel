fef6e9ff8be890424a881f23690087ff1a9a77e9
fschrader1992 <fschrader1992@gmail.com>
2020-04-28 19:46:24 +0200

    [script] Fix Transition Errors Murakami Sigma Midget Only Network

--- code/murakami_2003_sigma_network_midgets.sh
+++ code/murakami_2003_sigma_network_midgets.sh
@@ -8,16 +8,16 @@ do
     #values of sigma (percent of normal value)
     for s in {0..150..50}
     do
-            for i in 0 #80
+            for i in 0
             do
               j=107
               echo simulation $i $j
 
-              python3 03_network/ms_network_only_m.py jitter_fem/${k}/zebra_std${s}_on${j}off${i} zebra_${k}_std${s}_on${j}off${i} 62. 16. 636 ${j} ${i}
-              python3 03_network/ms_network_only_m.py jitter_fem/$((k+1))/zebra_std${s}_on${j}off${i} zebra_$((k+1))_std${s}_on${j}off${i} 62. 16. 636 ${j} ${i}
+              python3 03_network/ms_network_only_m.py murakami2003/${k}/zebra_std${s}_on${j}off${i} zebra_${k}_std${s}_on${j}off${i} 62. 16. 636 ${j} ${i}
+              python3 03_network/ms_network_only_m.py murakami2003/$((k+1))/zebra_std${s}_on${j}off${i} zebra_$((k+1))_std${s}_on${j}off${i} 62. 16. 636 ${j} ${i}
 
-              python3 03_network/ms_network_only_m.py jitter_fem/${k}/zebra_std$((s+25))_on${j}off${i} zebra_${k}_std$((s+25))_on${j}off${i} 62. 16. 636 ${j} ${i}
-              python3 03_network/ms_network_only_m.py jitter_fem/$((k+1))/zebra_std$((s+25))_on${j}off${i} zebra_$((k+1))_std$((s+25))_on${j}off${i} 62. 16. 636 ${j} ${i}
+              python3 03_network/ms_network_only_m.py murakami2003/${k}/zebra_std$((s+25))_on${j}off${i} zebra_${k}_std$((s+25))_on${j}off${i} 62. 16. 636 ${j} ${i}
+              python3 03_network/ms_network_only_m.py murakami2003/$((k+1))/zebra_std$((s+25))_on${j}off${i} zebra_$((k+1))_std$((s+25))_on${j}off${i} 62. 16. 636 ${j} ${i}
 
             done
 
